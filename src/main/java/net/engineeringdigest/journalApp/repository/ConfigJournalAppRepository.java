cae1d4c146d6a2429ccc49d8a94e122ac5e393ed
hitanshu0729 <hitanshu0729@gmail.com>
2025-01-17 00:20:03 +0530

    minor refactoring

--- src/main/java/net/engineeringdigest/journalApp/repository/ConfigJournalAppRepository.java
+++
@@ -1,8 +0,0 @@
-package net.engineeringdigest.journalApp.repository;
-
-import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ConfigJournalAppRepository extends MongoRepository<ConfigJournalAppEntity, ObjectId> {
-
-}
