5285a93ee5e7ab9f79330c387d0dc2e3b406891b
hitanshu0729 <hitanshu0729@gmail.com>
2025-01-17 00:18:44 +0530

    application post

--- src/main/java/net/engineeringdigest/journalApp/controller/ApplicationController.java
+++ src/main/java/net/engineeringdigest/journalApp/controller/ApplicationController.java
@@ -1,18 +1,19 @@
 package net.engineeringdigest.journalApp.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import net.engineeringdigest.journalApp.entity.Application;
 import net.engineeringdigest.journalApp.service.ApplicationService;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 @RequestMapping("/application")
 @RestController
+@Slf4j
 public class ApplicationController {
 
     @Autowired
@@ -27,4 +28,21 @@ public class ApplicationController {
     public void deleteApplication(String id) {
         applicationService.deleteApplication(new ObjectId(id));
     }
+
+    @PostMapping("/create")
+    public ResponseEntity<?> createApplication(
+            @RequestParam("resume") MultipartFile resume, // File input
+            @RequestParam("name") String name,  // Other application details
+            @RequestParam("email") String email,
+            @RequestParam("coverLetter") String coverLetter,
+            @RequestParam("phone") String phone,
+            @RequestParam("address") String address,
+            @RequestParam("jobId") String jobId
+    ){
+        if (resume == null || resume.isEmpty()) {
+            return ResponseEntity.badRequest().body("Resume File Required!");
+        }
+        return applicationService.createApplication(resume, name, email, coverLetter, phone, address, new ObjectId(jobId));
+    }
+
 }
--- src/main/java/net/engineeringdigest/journalApp/service/ApplicationService.java
+++ src/main/java/net/engineeringdigest/journalApp/service/ApplicationService.java
@@ -1,8 +1,12 @@
 package net.engineeringdigest.journalApp.service;
-
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import lombok.extern.slf4j.Slf4j;
 import net.engineeringdigest.journalApp.entity.Application;
+import net.engineeringdigest.journalApp.entity.Job;
 import net.engineeringdigest.journalApp.entity.User;
 import net.engineeringdigest.journalApp.repository.ApplicationRepository;
+import net.engineeringdigest.journalApp.repository.JobRepository;
 import net.engineeringdigest.journalApp.repository.UserRepository;
 import org.apache.coyote.Response;
 import org.bson.types.ObjectId;
@@ -11,11 +15,21 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 @Configuration
+@Slf4j
 public class ApplicationService {
+
+    @Autowired
+    private Cloudinary cloudinary;
+
+    @Autowired
+    private JobRepository jobRepository;
 
     @Autowired
     private UserRepository userRepository;
@@ -49,4 +63,60 @@ public class ApplicationService {
         }
         return ResponseEntity.badRequest().body("You are not authorized to delete this application");
     }
+
+    public ResponseEntity<?> createApplication(MultipartFile resume,  String name, String email, String coverLetter, String phone, String address,ObjectId jobId){
+        try{
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            String userEmail = authentication.getName();  // This will return the email if the subject is set to email
+            User user = userRepository.findByEmail(userEmail);
+            if(user == null || !user.getRole().contains("Job Seeker")){
+                return ResponseEntity.badRequest().body("Unauthorized");
+            }
+            if (!Arrays.asList("image/png", "image/jpeg", "image/webp", "application/pdf")
+                    .contains(resume.getContentType())) {
+                return ResponseEntity.badRequest().body("Invalid file type. Please upload a PNG, JPEG, WEBP, or PDF file.");
+            }
+            if (name == null || name.trim().isEmpty() ||
+                    email == null || email.trim().isEmpty() ||
+                    coverLetter == null || coverLetter.trim().isEmpty() ||
+                    phone == null || phone.trim().isEmpty() ||
+                    address == null || address.trim().isEmpty()) {
+                return ResponseEntity.badRequest().body("Please fill all fields");
+            }
+            if (!phone.matches("\\d+")) {
+                return ResponseEntity.badRequest().body("Invalid phone number format");
+            }
+
+            Job job = jobRepository.findById(jobId).orElse(null);
+            if(job == null){
+                return ResponseEntity.badRequest().body("Job not found");
+            }
+            Map<String, Object> uploadResult = cloudinary.uploader().upload(resume.getBytes(),
+                    ObjectUtils.asMap("resource_type", "auto"));
+            if (uploadResult == null || uploadResult.isEmpty()) {
+                return ResponseEntity.badRequest().body("Error occurred while uploading resume");
+            }
+            String secureUrl = (String) uploadResult.get("secure_url");
+            String publicId = (String) uploadResult.get("public_id");
+            if (publicId == null || publicId.isEmpty() || secureUrl == null || secureUrl.isEmpty()) {
+                return ResponseEntity.badRequest().body("Error occurred while uploading resume");
+            }
+
+            Application application = new Application();
+            application.setName(name);
+            application.setEmail(email);
+            application.setCoverLetter(coverLetter);
+            application.setPhone(Long.parseLong(phone));
+            application.setAddress(address);
+            application.setResume(new Application.Resume(publicId, secureUrl));
+            application.setApplicant(user);
+            application.setEmployer(userRepository.findById(job.getPostedBy()).orElse(null));
+            applicationRepository.save(application);
+        } catch (Exception e) {
+            log.error("An unexpected error occurred while processing the application: ", e);
+            return ResponseEntity.badRequest().body("An unexpected error occurred while processing the application");
+        }
+        return ResponseEntity.ok("Application submitted successfully");
+    }
+
 }
